d291182420d80fe52fe98306d3f1c456b65dd202
Sergei Fedorov <s.fedorov.mhi@yandex.ru>
2023-09-09 23:19:27 +0300

    refactoring Flake

--- app/database.py
+++ app/database.py
@@ -17,7 +17,11 @@ DATABASE_URL = "sqlite+aiosqlite:///../app.db"
 engine = create_async_engine(DATABASE_URL, echo=True)
 # expire_on_commit=False will prevent attributes from being expired
 # after commit.
-async_session = async_sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+async_session = async_sessionmaker(
+    engine,
+    expire_on_commit=False,
+    class_=AsyncSession
+)
 
 
 session = async_session()
